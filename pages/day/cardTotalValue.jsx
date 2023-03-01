9ff36e7a80bf674650ba53c4497de49ffa3452c9
frazzZFRA <120016143+frazzZFRA@users.noreply.github.com>
2023-03-01 17:41:46 +0100

    Initial commit

---
+++ pages/day/cardTotalValue.jsx
@@ -0,0 +1,12 @@
+export default function Account() {
+  return (
+    <img
+      className="account"
+      src="https://i.imgur.com/1bX5QH6.jpg"
+      class="rounded-full border-none"
+      alt="Lin Lanying"
+      width={100}
+      height={100}
+    />
+  );
+}
---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Profile from "./day/cardTotalValue";
+import App from "./day/trackPortFolio";
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <h2>Jonh Cena</h2>
+        <Profile />
+      </div>
+      <App />;
+    </>
+  );
+}
