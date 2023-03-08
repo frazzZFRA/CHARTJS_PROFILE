9688b36beca804c25968abb0ae95c897bd0f1d47
frazzZFRA <120016143+frazzZFRA@users.noreply.github.com>
2023-03-08 02:19:35 +0100

    new index.js

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,12 +4,16 @@ import App from "./day/trackPortFolio";
 
 export default function Home() {
   return (
-    <>
-      <div>
-        <h2>Jonh Cena</h2>
-        <Profile />
-      </div>
-      <App />;
-    </>
+   <div className="relative flex flex-wrap items-center align-top justify-start bg-slate-800 rounded-md p-9">
+                
+                <Account/>Name Surname
+                
+                <ProfitLoss/>
+                <div className="w-full height-10  ">
+                 {/* <App/> */}
+                </div>
+                <CloseTokenAccount/>
+                
+            </div>
   );
 }
