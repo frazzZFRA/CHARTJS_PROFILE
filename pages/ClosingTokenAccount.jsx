368aa140bb032adf6eb6cdb972942f02fae1d401
frazzZFRA <120016143+frazzZFRA@users.noreply.github.com>
2023-03-08 02:18:04 +0100

    new components

---
+++ pages/ClosingTokenAccount.jsx
@@ -0,0 +1,5 @@
+export default function CloseTokenAccount(/* Parse from json file account.name*/) {
+    return (
+        <div className="w-full flex flex-col items-center rounded-md bg-gray-800 object-contain h-44 justify-between px-3 py-2 shadow relative border-2 cursor-pointer border-transparent">ACCOUNT</div>
+        )}
+        
---
+++ pages/ProfitLossCard.tsx
@@ -0,0 +1,4 @@
+export default function ProfitLoss(/* Parse from json file account.name*/) {
+    return (
+            <div className="absolute top-0 right-20 w-56 h-18 max-w p-8 bg-light-grey rounded-lg text-lime-600 text-6xl text-center">50%</div>
+    )}
